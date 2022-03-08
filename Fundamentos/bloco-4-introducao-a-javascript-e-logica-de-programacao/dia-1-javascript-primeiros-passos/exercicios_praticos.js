4a94e73a3c91684beb1449311da491218ae4807f
Maristela Santos <maristela_sp@hotmail.com>
2022-03-08 13:07:10 +0400

    adiciona parte 2 do exercicio

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios_praticos.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios_praticos.js
@@ -1,7 +1,7 @@
 const a = 2;
 const b = 4;
 
-// Parte 1
+//Parte 1
 // let soma = a + b;
 // console.log ("Soma: " + (soma));
 // let subtracao = a - b; 
@@ -13,7 +13,7 @@ const b = 4;
 // let modulo = a % b; 
 // console.log ("Modulo: " + (modulo)); 
 
-//Parte 2
+// //Parte 2
 if (a > b) {
     console.log ("Então A é maior");
 } else {
