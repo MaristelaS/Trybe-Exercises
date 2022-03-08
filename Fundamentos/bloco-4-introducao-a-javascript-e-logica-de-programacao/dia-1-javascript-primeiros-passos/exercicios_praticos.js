d786ffa5e87f0dcb1ac5a3d081b853668eb40d13
Maristela Santos <maristela_sp@hotmail.com>
2022-03-08 13:05:25 +0400

    exercicio parte 1

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios_praticos.js
@@ -0,0 +1,21 @@
+const a = 2;
+const b = 4;
+
+// Parte 1
+// let soma = a + b;
+// console.log ("Soma: " + (soma));
+// let subtracao = a - b; 
+// console.log ("Subtração: " + (subtracao));
+// let multiplicacao = a * b; 
+// console.log ("Multiplicação: " + (multiplicacao));
+// let divisao = a / b; 
+// console.log ("Divisão: " + (divisao));
+// let modulo = a % b; 
+// console.log ("Modulo: " + (modulo)); 
+
+//Parte 2
+if (a > b) {
+    console.log ("Então A é maior");
+} else {
+    console.log ("Então B é maior");
+}
