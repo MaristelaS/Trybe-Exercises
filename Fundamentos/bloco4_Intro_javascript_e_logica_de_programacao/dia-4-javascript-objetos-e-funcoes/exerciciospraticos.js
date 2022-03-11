8a5ed01c0f81decd7bcf982f4be67d4b9d49769d
Maristela Santos <maristela_sp@hotmail.com>
2022-03-11 20:57:04 +0400

    organiza diretorios

---
+++ Fundamentos/bloco4_Intro_javascript_e_logica_de_programacao/dia-4-javascript-objetos-e-funcoes/exerciciospraticos.js
@@ -0,0 +1,10 @@
+let player = {
+name: 'Marta',
+lastName: 'Silva',  
+age: 34,
+medals: { golden: 2, silver: 3 },
+}
+//Acesse as chaves name , lastName e age e concatene as suas informações para imprimir no console uma mensagem no seguinte formato: "A jogadora Marta Silva tem 34 anos de idade".
+console.log ("A jogadora ", player.name, player.lastName, "tem ", player.age, "anos de idade");
+
+//Adicione ao objeto a chave bestInTheWorld e atribua a esta chave um array contendo as datas em que a jogadora Marta foi considerada a melhor do mundo.
